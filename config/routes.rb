0469f7b7a685151a98ab9281a7fb87c4d276b2ed
rlandows <rlandows@uci.edu>
2017-02-02 09:27:00 -0800

    checkpoint-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  get 'welcome/about'
+  resources :wikis
 
-  root 'welcome#index'
+  devise_for :users
+  get 'about' => 'welcome#about'
+
+  get 'index' => 'welcome#index'
+
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do
+  Wiki.create!(
+  title: "RandomData.random_sentence",
+  body: "RandomData.random_paragraph",
+  private: false,
+  user: nil
+  )
+end
+
+puts "#{Wiki.count}"
