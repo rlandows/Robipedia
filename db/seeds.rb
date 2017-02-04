3d332ce79bd991c7b74720973d329cc301f032a5
rlandows <rlandows@uci.edu>
2017-02-04 14:52:23 -0800

    adding faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
 
 100.times do
   Wiki.create!(
-  title: "RandomData.random_sentence",
-  body: "RandomData.random_paragraph",
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraph,
   private: false,
   user: nil
   )
 end
+wikis = Wiki.all
 
-puts "#{Wiki.count}"
+50.times do
+  user = User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+
+users = User.all
+
+
+  admin = User.create!(
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+
+
+  member = User.create!(
+  email:    'member@example.com',
+  password: 'helloworld'
+  )
+
+
+  premium = User.create!(
+    email:    'premium@example.com',
+    password: 'helloworld',
+    role:     'premium'
+  )
+
+puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
